c6ec940ec1a1c4440ed00eba21796941eef9b867
mahendra1375 <mahendragaikwad275@gmail.com>
2025-03-08 13:36:25 +0530

    Test changes4

--- src/test/java/StepDefination/APISteps.java
+++ src/test/java/StepDefination/APISteps.java
@@ -59,7 +59,7 @@ public class APISteps {
         System.out.println("Server in Response Header: "+Server_val);
 
         System.out.println("kishor1");
-        System.out.println("kishor3");
+        System.out.println("kishor8");
         System.out.println("Wabale");
 
     }
